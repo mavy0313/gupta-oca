4c1ad10739319d2b62f06ab9d9433464e8cae675
mavy0313 <vypoff@gmail.com>
2019-11-21 07:27:43 +0400

    Constructor overloading

---
+++ src/main/java/chapter3/ConstructorOverloading.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class ConstructorOverloading {
+
+    public ConstructorOverloading(int i) {
+    }
+
+//    private ConstructorOverloading(int i) { already defined
+//    }
+
+    ConstructorOverloading(String s) {
+    }
+}
