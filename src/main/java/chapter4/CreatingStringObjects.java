ef5e2cb7659064ebf7fb794854bd57ff326ef5d7
mavy0313 <vypoff@gmail.com>
2019-12-05 02:20:24 +0400

    Creating String objects

---
+++ src/main/java/chapter4/CreatingStringObjects.java
@@ -0,0 +1,28 @@
+package chapter4;
+
+public class CreatingStringObjects {
+    public static void main(String[] args) {
+        String str1 = new String("Paul");
+        String str2 = new String("Paul");
+        System.out.println(str1 == str2); //false
+
+        String str3 = "Harry";
+        String str4 = "Harry";
+        System.out.println(str3 == str4); //true, same object in String pool
+
+        String morning1 = "Morning"; //placed to String pool
+        System.out.println("Morning" == morning1); //true
+
+        String morning2 = new String("Morning");//not placed to String pool
+        System.out.println("Morning" == morning2); //false
+
+        String girl = new String("Shreya");
+        char[] name = new char[]{'P','a','u','l'};
+        String boy = new String(name);
+
+        StringBuilder sb1 = new StringBuilder("String Builder");
+        String str5 = new String(sb1);
+        StringBuffer sb2 = new StringBuffer("String Buffer");
+        String str6 = new String(sb2);
+    }
+}
